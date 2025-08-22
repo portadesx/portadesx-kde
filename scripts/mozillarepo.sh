152545629edcef3cbef4d7f4eaa65e56080cd0ff
Alif <arfshl@protonmail.com>
2025-08-22 19:12:30 +0700

    Create mozillarepo.sh

---
+++ scripts/mozillarepo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+cat <<EOF > /etc/apt/sources.list.d/mozilla.sources
+Types: deb
+URIs: https://packages.mozilla.org/apt
+Suites: mozilla
+Components: main
+Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc
+EOF
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+apt update
